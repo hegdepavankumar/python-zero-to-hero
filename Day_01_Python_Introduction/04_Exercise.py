06573778abfe1e65a67c496d332fca1aea77fc6d
Pavankumar Hegde <85627085+hegdepavankumar@users.noreply.github.com>
2023-04-16 06:47:45 +0530

    Update 04_Exercise.py

--- Day_01_Python_Introduction/04_Exercise.py
+++ Day_01_Python_Introduction/04_Exercise.py
@@ -1,1 +1,76 @@
+ # Comments in Python are written with a special character, which one?
+  #  __This is a comment
+  
+  
+# declare and initialize two variables
+num1 = 6
+num2 = 9
+
+# print the output
+print('This is output')
+
+
+# create a variable
+name = 'Pavankumar'
+
+# print the value
+print(name)
+# output Pavankumar
+
+
+name = 'Pavankumar' # name is a string
+
+
+# This is a long comment
+# and it extends
+# to multiple lines
+
+''' This is also a
+perfect example of
+multi-line comments '''
+
+
+
+print('Python')
+
+# print('Error Line )
+
+print('Django')
+
+
+
+
+
+# Python program to demonstrate
+# multiline comments
+print("Multiline comments")
+
+
+
+""" Python program to demonstrate
+ multiline comments"""
+print("Multiline comments")
+
+
+
+def multiply(a, b):
+    """Multiplies the value of a and b"""
+    return a*b
+ 
+# Print the docstring of multiply function
+print(multiply.__doc__)
+
+
+
+"""
+You may use this tutorial freely at your own risk. See [LICENSE](https://github.com/hegdepavankumar/python-zero-to-hero/blob/main/LICENSE). <br>
+
+| [Next](https://github.com/hegdepavankumar/python-zero-to-hero/tree/master/Day_02_Variables_%26_Data_Types) | [List of contents](https://github.com/hegdepavankumar/python-zero-to-hero/blob/master/list_of_contents.md) |
+"""
+
+
+
+
+
+
 
